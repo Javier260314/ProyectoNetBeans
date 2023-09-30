5bae5ec400efa258ae00f009b2708ad9a7912f2a
Javier <Javier@DESKTOP-LKTTV91>
2023-09-30 17:08:46 +0200

    Hola Javier

---
+++ src/main/java/com/mycompany/proyectonetbeans/ProyectoNetBeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectonetbeans;
+
+/**
+ *
+ * @author Javier
+ */
+public class ProyectoNetBeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Javier!");
+    }
+}
